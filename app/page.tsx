971b11cda20be4dab41e78064b742c0dbe55fd60
rattlesnaked <anvosdiah@gmail.com>
2024-12-27 06:48:46 +0000

    init

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import ListPage from "@/client/ListPage";
+
+export default function Home() {
+  return (
+    <div className="flex p-24 h-[100dvh] w-[100dvw] bg-slate-200">
+      <ListPage/>
+    </div>
+  );
+}
